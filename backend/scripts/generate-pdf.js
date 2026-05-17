9f3f922abc60b42e67c18fe3930e2ac0106b9c25
zsombor <komlos.zsombor@gmail.com>
2026-05-17 11:40:45 +0200

    PDF download fix

--- backend/scripts/generate-pdf.js
+++ backend/scripts/generate-pdf.js
@@ -9,7 +9,7 @@ if (!url || !outputPath || !executablePath) {
 
 const browser = await puppeteer.launch({
     executablePath,
-    args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--disable-crash-reporter'],
     headless: true,
 });
 
